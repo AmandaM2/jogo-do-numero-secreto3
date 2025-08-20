d6713330871893259c8d7a749e1b48e0a666bc85
AmandaM2 <aaamnd112@gmail.com>
2025-08-20 10:41:14 -0300

    terminei o botao de reiniciar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+let listaDeNumerosSorteados = []
 let numeroSecreto = gerarNumeroAleatorio ();
-let tentativas = 1
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -11,11 +12,7 @@ function exibirMensagemInicial(){
     exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 }
 
-function gerarNumeroAleatorio(){
-   return parseInt(Math.random()*10+1);
-}
-
-function exibirMensagemInicial()
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -38,6 +35,20 @@ function verificarChute() {
 }
 }
 
+function gerarNumeroAleatorio(){
+   let numeroEscolhido = parseInt(Math.random()*10+1);
+   if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+   return gerarNumeroAleatorio;
+    } else {
+    listaDeNumerosSorteados.push(numeroEscolhido)
+    console.log(listaDeNumerosSorteados)
+    return numeroEscolhido;
+   }
+}
+
+
+
+
 function limparTela(){
     chute = document.querySelector('input');
     chute.value = '';
@@ -45,9 +56,9 @@ function limparTela(){
 
 function reiniciarJogo(){
   numeroSecreto = gerarNumeroAleatorio();
-  limparCampo();
+  limparTela();
   tentativas = 1;
   exibirMensagemInicial();
-  document.getElementById('reiniciar')
+  document.getElementById('reiniciar').setAttribute('disabled',true);
   
 }
